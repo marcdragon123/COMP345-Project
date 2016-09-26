d5b32dfc9169fc256314610ebbd77d40117a677a
marcdragon123 <thedragon_master@hotmail.ca>
2016-09-26 16:59:48 -0400

    Assignment 1: Dice part 4

---
+++ Dice8.cpp
@@ -0,0 +1,12 @@
+#include "Dice.h"
+
+//This is the class of an eight sided die.
+
+void Dice8::Roll() {
+	Dice::Roll(8);
+}
+
+int Dice8::getCurrentRoll()
+{
+	return Dice::getCurrentRoll();
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//This header includes all the standard librairies used by all the files.
+
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <time.h>
+#include <tchar.h>
+#include <cmath>
